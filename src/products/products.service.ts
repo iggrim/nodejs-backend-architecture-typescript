4085de0394133842ff57c1b5eb85724e3c3fd26f
Mig <mlg-case@mail.ru>
2022-09-27 20:26:33 +0300

    fix dependency injection

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,21 @@
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types';
 import { IProductsService } from "./products.service.interface";
 import { Product } from "./product.entity";
 import { ProductsRepository} from './products.repository'
 import { v4 as uuidv4 } from 'uuid';
 
+
+@injectable()
 export class ProductsService implements IProductsService{
+  constructor(
+    @inject(TYPES.ProductsRepository) private productsRepository: ProductsRepository
+  ){ }
   async createProduct({title, price, img }: Product): Promise<Product>{
     const id = uuidv4();
     const newProduct = new Product(title, price, img, id);
     
-    const newRepository = new ProductsRepository();
+    const newRepository = this.productsRepository;
     await newRepository.save(newProduct);
 
     return newProduct;
